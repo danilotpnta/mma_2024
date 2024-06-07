7e7d769b7cb0949d41e8a9591f6d51c5e1016859
Danilo Toapanta <70687643+danilotpnta@users.noreply.github.com>
2024-06-07 15:40:21 +0200

    catch corrupted MP3 files in conversion

--- src/utils/audio_utils.py
+++ src/utils/audio_utils.py
@@ -1,5 +1,14 @@
 from pydub import AudioSegment
+import logging
+
+# Catch corrupted files into a txt file
+logging.basicConfig(filename='conversion_errors.log', level=logging.ERROR)
 
 def convert_mp3_to_wav(input_filename, output_filename):
-    audio = AudioSegment.from_mp3(input_filename)
-    audio.export(output_filename, format="wav")
+    try:
+        audio = AudioSegment.from_mp3(input_filename)
+        audio.export(output_filename, format="wav")
+    except Exception as e:
+        error_message = f"Error converting {input_filename}: {e}"
+        print(error_message)
+        logging.error(error_message)
