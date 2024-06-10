20b9167d4728b41756ddd460bc69aa564788abb8
Danilo Toapanta <70687643+danilotpnta@users.noreply.github.com>
2024-06-10 18:34:45 +0200

    feat: add early stopping form https://lightning.ai/docs/pytorch/stable/cli/lightning_cli_advanced_3.html

--- src/train_lightning.py
+++ src/train_lightning.py
@@ -1,7 +1,3 @@
-import os
-from torch import optim, nn, utils, Tensor
-from torchvision.datasets import MNIST
-from torchvision.transforms import ToTensor
 import lightning as L
 from lightning.pytorch.cli import LightningCLI
 from lightning.pytorch.callbacks.early_stopping import EarlyStopping
@@ -9,10 +5,10 @@ from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 from models.fcn_classifier import GenreFCN
 
 def cli_main():
-    early_stopping = EarlyStopping('val_loss')
-    cli = LightningCLI(GenreFCN, DataPlaceholder)
-                    #    Todo: add early stopping
-                    #    trainer_defaults={})
+    
+    early_stopping = EarlyStopping(monitor='val_loss', patience=3, verbose=True, mode='min')
+    cli = LightningCLI(GenreFCN, DataPlaceholder, trainer_defaults={"callbacks": [early_stopping]})
+
 
 
 if __name__ == "__main__":
