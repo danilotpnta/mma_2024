a33bd22e2ffdafa1cbf336bd0ae38e53f2712f95
oliverneut <oliver.neut@gmail.com>
2024-06-21 16:54:00 +0200

    t-SNE and UMAP refactor

--- dashboard/src/widgets/projection_radio_buttons.py
+++ dashboard/src/widgets/projection_radio_buttons.py
@@ -5,7 +5,7 @@ import config
 def create_projection_radio_buttons():
 
     return dbc.RadioItems(
-        options=[{"label": x, "value": x} for x in ["UMAP", "t-SNE"]],
+        options=[{"label": label, "value": value} for (label, value) in [('UMAP', 'umap'), ('t-SNE', 'tsne')]],
         value=config.DEFAULT_PROJECTION,
         inline=True,
         id="projection-radio-buttons",
