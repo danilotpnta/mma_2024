d1fc5169117208173c2fb39889fc2dc709ec0ef9
gregorygo12 <gregorygocc@gmail.com>
2024-06-27 23:13:28 +0200

    fix: polish

--- dashboard/src/widgets/deselect_button.py
+++ dashboard/src/widgets/deselect_button.py
@@ -1,37 +1,56 @@
-from dash import html, dcc, Input, Output, State, callback, no_update, ALL, callback_context
 import dash_bootstrap_components as dbc
-from Collection import Collection
+from dash import (
+    html,
+    dcc,
+    Input,
+    Output,
+    State,
+    callback,
+    no_update,
+    ALL,
+    callback_context,
+)
+
 from widgets import (
     scatterplot_2d,
     scatterplot_3d,
     track_info,
     categorical_histogram,
     numerical_histogram,
-    filter_view 
-    )
+    filter_view,
+)
+from Collection import Collection
 from utils.utils import feature_key_from_state_string
 
+
 def create_deselect_button():
-    return dbc.Button('Deselect All', id='deselect-button')
+
+    return dbc.Button("Deselect All", id="deselect-button")
+
 
 @callback(
-    Output('scatterplot-2D', 'figure'),
-    Output('scatterplot-3D', 'figure'),
-    Output('track-info', 'children'),
-    Output({"type": 'histogram', 'feature': ALL}, "figure", allow_duplicate=True),
-    Output('filter-view', 'children'),
-    Input('deselect-button', 'n_clicks'),
-    State('projection-radio-buttons', 'value'),
-    State({"type": 'histogram', 'feature': ALL}, "figure"),
-    prevent_initial_call=True
+    Output("scatterplot-2D", "figure"),
+    Output("scatterplot-3D", "figure"),
+    Output("track-info", "children"),
+    Output({"type": "histogram", "feature": ALL}, "figure", allow_duplicate=True),
+    Output("filter-view", "children"),
+    Input("deselect-button", "n_clicks"),
+    State("projection-radio-buttons", "value"),
+    State({"type": "histogram", "feature": ALL}, "figure"),
+    prevent_initial_call=True,
 )
 def deselect_all(n_clicks, radio_button_value, _):
+
     print(n_clicks)
     if n_clicks is None:
         return no_update, no_update, no_update
     else:
 
-        hist_dict = {feature_key_from_state_string(k): v for k, v in callback_context.states.items() if 'histogram' in k}
+        hist_dict = {
+            feature_key_from_state_string(k): v
+            for k, v in callback_context.states.items()
+            if "histogram" in k
+        }
 
         Collection.load()
 
@@ -39,12 +58,17 @@ def deselect_all(n_clicks, radio_button_value, _):
         fig_3d = scatterplot_3d.create_scatterplot_figure(radio_button_value)
         track_info_widget = track_info.create_track_info_widget()
 
-        hist_dict['genre'] = categorical_histogram.draw_histogram('genre')
-        hist_dict['tempo'] = numerical_histogram.draw_histogram('tempo', nbins=30)
-        hist_dict['key'] = categorical_histogram.draw_histogram('key')
-        hist_dict['loudness'] = numerical_histogram.draw_histogram('loudness', nbins=20)
-
+        hist_dict["genre"] = categorical_histogram.draw_histogram("genre")
+        hist_dict["tempo"] = numerical_histogram.draw_histogram("tempo", nbins=30)
+        hist_dict["key"] = categorical_histogram.draw_histogram("key")
+        hist_dict["loudness"] = numerical_histogram.draw_histogram("loudness", nbins=20)
 
         filter_widget = filter_view.draw_filter_view()
 
-        return fig_2d, fig_3d, track_info_widget, list(hist_dict.values()), filter_widget
+        return (
+            fig_2d,
+            fig_3d,
+            track_info_widget,
+            list(hist_dict.values()),
+            filter_widget,
+        )
--- dashboard/src/widgets/filter_view.py
+++ dashboard/src/widgets/filter_view.py
@@ -2,25 +2,34 @@ import dash_bootstrap_components as dbc
 from dash import html
 from typing import List, Tuple
 
-
+# Color dictionary
 feature_colors = {
-    'genre': 'primary',
-    'tempo': 'success',
-    'key': 'warning',
-    'loudness': 'danger'
+    "genre": "primary",
+    "tempo": "success",
+    "key": "warning",
+    "loudness": "danger",
 }
 
 
-def draw_filter_view(filters: List[Tuple[str, str]]=[]):
-    buttons = [dbc.Button(
-        f"{feature}:{value[1]}", color=feature_colors[feature],
-        className='filter-button') for feature, value in filters
-        ]
+def draw_filter_view(filters: List[Tuple[str, str]] = []):
+
+    buttons = [
+        dbc.Button(
+            f"{feature}:{value[1]}",
+            color=feature_colors[feature],
+            className="filter-button",
+        )
+        for feature, value in filters
+    ]
 
     return buttons
 
-def create_filter_view(filters: List[Tuple[str, str]]=[]):
+
+def create_filter_view(filters: List[Tuple[str, str]] = []):
+
     buttons = draw_filter_view(filters)
-    
-    filter_view = html.Div(children=buttons, id='filter-view', className='d-flex flex-wrap gap-2')
+    filter_view = html.Div(
+        children=buttons, id="filter-view", className="d-flex flex-wrap gap-2"
+    )
+
     return filter_view
